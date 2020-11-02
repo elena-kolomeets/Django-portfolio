aac4fb40c7f77e14fd4125ae9093db36f2d40225
Elena <kolomeets.elena7@gmail.com>
2020-11-02 19:09:31 +0100

    First commit, Portfolio and Blog drafts

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from datetime import date
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(default=date.today)
+    text = models.TextField()
+    url = models.URLField(blank=True)
